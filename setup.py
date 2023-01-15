99b04119848326d1a401600ca17c74cd753bfa98
akshaymane920 <79091182+akshaymane920@users.noreply.github.com>
2023-01-15 11:37:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name='pyFortiManagerAPI',
     description='A Python wrapper for the FortiManager REST API',
-    version='0.1.9',
+    version='0.2.0',
     py_modules=["pyFortiManagerAPI"],
     package_dir={'': 'src'},
     keywords=['FortiManager', 'RestAPI', 'API', 'FortiGate', 'Fortinet', "python", "FortiManager API",
