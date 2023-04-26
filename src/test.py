d4bf23f20fbb53d6ecd8790a9333e02334b1a3be
Allen <3464997+drekio@users.noreply.github.com>
2023-04-26 10:49:32 -0400

    Delete test.py

--- src/test.py
+++
@@ -1,5 +0,0 @@
-import pyFortiManagerAPI
-
-fortimgr = pyFortiManagerAPI.FortiManager(host="fortimanager.zaxbys.com",username="zaxapi",password="gFJqbQ2hiiWrLNb33vM",adom="root",verify=False)
-
-print(fortimgr.get_script_output("ZAX-ORC01-FW01", "Corp_Stores"))
